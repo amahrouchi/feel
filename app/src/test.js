01775ec312341c345dfab75105bef2cd6b681939
Angelo Mahrouchi <angelo.mahrouchi@omictools.com>
2018-07-04 13:56:05 +0200

    Test the app build

---
+++ app/src/test.js
@@ -0,0 +1,11 @@
+class A {
+    test () {
+        console.log('test');
+        console.log('angelo');
+        console.log('angelo2');
+        console.log('angelo3');
+    }
+}
+
+let a = new A();
+a.test();
