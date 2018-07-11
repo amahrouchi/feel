0af4521ade3f709bfa656ee8f9e588a027d06fca
Angelo Mahrouchi <angelo.mahrouchi@omictools.com>
2018-07-11 13:39:19 +0200

    TS works

--- app/src/tests/test2.ts
+++ app/src/tests/test2.ts
@@ -1,5 +1,6 @@
 export class B {
     test () {
         console.log('class B');
+        console.log('class B2');
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,23 @@
-let path    = require('path');
-let webpack = require('webpack');
-
-// Phaser
-let phaserModule = path.join(__dirname, '/node_modules/phaser/');
-let phaser = path.join(phaserModule, 'dist/phaser.min.js');
+const path = require('path');
 
 module.exports = {
-    mode    : 'development',
-    entry   : './app/src/app.js',
+    entry   : './app/src/app.ts',
+    devtool : 'inline-source-map',
+    watch : true,
     output  : {
-        path     : path.resolve(__dirname, 'app/public/js'),
-        filename : 'app.bundle.js'
+        filename : 'app.bundle.js',
+        path     : path.resolve(__dirname, 'app/public/js')
     },
     module  : {
         rules : [
             {
-                test    : /\.js$/,
-                exclude : /node_modules/,
-                use     : 'babel-loader'
-            },
-            {
-                test: /phaser\.min\.js$/,
-                use: ['expose-loader?Phaser']
+                test    : /\.tsx?$/,
+                use     : 'ts-loader',
+                exclude : /node_modules/
             }
         ]
     },
-
-    resolve: {
-        // alias to import modules in ES6 JS scripts
-        alias: {
-            'phaser': phaser,
-        }
+    resolve : {
+        extensions : ['.tsx', '.ts', '.js']
     },
-    stats   : {
-        colors : true
-    },
-    devtool : 'source-map',
-    watch   : true
 };
