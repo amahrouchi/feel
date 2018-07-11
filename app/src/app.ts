b042486424f13404b8c4e923c00ba5a756294c2c
Angelo Mahrouchi <angelo.mahrouchi@omictools.com>
2018-07-11 14:16:40 +0200

    TS + Phaser build works

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,8 +1,9 @@
-/* ******** */
-/* TEST ES6 */
-/* ******** */
+/* *************** */
+/* TEST TypeScript */
+/* *************** */
 
-// import Phaser from 'phaser';
+/// <reference path="types/phaser.d.ts"/>
+import 'phaser';
 import {A} from './tests/test';
 import {B} from './tests/test2';
 
@@ -12,54 +13,53 @@ a.test();
 let b = new B();
 b.test();
 
-// let config = {
-//     type: Phaser.AUTO,
-//     width: 800,
-//     height: 600,
-//     physics: {
-//         default: 'arcade',
-//         arcade: {
-//             gravity: { y: 200 }
-//         }
-//     },
-//     scene: {
-//         preload: preload,
-//         create: create
-//     }
-// };
-//
-// /* *********** */
-// /* TEST PHASER */
-// /* *********** */
-//
-// let game = new Phaser.Game(config);
-//
-// function preload ()
-// {
-//     this.load.setBaseURL('http://labs.phaser.io');
-//
-//     this.load.image('sky', 'assets/skies/space3.png');
-//     this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-//     this.load.image('red', 'assets/particles/red.png');
-// }
-//
-// function create ()
-// {
-//     this.add.image(400, 300, 'sky');
-//
-//     let particles = this.add.particles('red');
-//
-//     let emitter = particles.createEmitter({
-//         speed: 100,
-//         scale: { start: 1, end: 0 },
-//         blendMode: 'ADD'
-//     });
-//
-//     let logo = this.physics.add.image(400, 100, 'logo');
-//
-//     logo.setVelocity(100, 200);
-//     logo.setBounce(1, 1);
-//     logo.setCollideWorldBounds(true);
-//
-//     emitter.startFollow(logo);
-// }
+/* *********** */
+/* TEST PHASER */
+/* *********** */
+let config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+let game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.setBaseURL('http://labs.phaser.io');
+
+    this.load.image('sky', 'assets/skies/space3.png');
+    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
+    this.load.image('red', 'assets/particles/red.png');
+}
+
+function create ()
+{
+    this.add.image(400, 300, 'sky');
+
+    let particles = this.add.particles('red');
+
+    let emitter = particles.createEmitter({
+        speed: 100,
+        scale: { start: 1, end: 0 },
+        blendMode: 'ADD'
+    });
+
+    let logo = this.physics.add.image(400, 100, 'logo');
+
+    logo.setVelocity(100, 200);
+    logo.setBounce(1, 1);
+    logo.setCollideWorldBounds(true);
+
+    emitter.startFollow(logo);
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,32 @@
-const path = require('path');
+const path         = require('path');
+const pathToPhaser = path.join(__dirname, '/node_modules/phaser/');
+const phaserFile   = path.join(pathToPhaser, 'dist/phaser.js');
 
 module.exports = {
     entry   : './app/src/app.ts',
     devtool : 'inline-source-map',
-    watch : true,
+    watch   : true,
     output  : {
         filename : 'app.bundle.js',
         path     : path.resolve(__dirname, 'app/public/js')
     },
+    resolve : {
+        extensions : ['.ts', '.js'],
+        alias      : {
+            phaser : phaserFile
+        }
+    },
     module  : {
         rules : [
             {
-                test    : /\.tsx?$/,
+                test    : /\.ts$/,
                 use     : 'ts-loader',
                 exclude : /node_modules/
+            },
+            {
+                test   : /phaser\.js$/,
+                loader : 'expose-loader?Phaser'
             }
         ]
-    },
-    resolve : {
-        extensions : ['.tsx', '.ts', '.js']
     },
 };
