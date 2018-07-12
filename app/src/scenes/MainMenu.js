ac8ef7e5e9d85aeacbeb5206c403b1df294618b3
Angelo Mahrouchi <angelo.mahrouchi@gmail.com>
2018-07-12 22:58:33 +0200

    Display the game menu

--- app/src/scenes/MainMenu.js
+++ app/src/scenes/MainMenu.js
@@ -8,31 +8,85 @@ export default class MainMenu extends AbstractScene
     /**
      * Constructor
      */
-    constructor () {
+    constructor() {
         super({'key' : 'MainMenu'})
     }
 
     /**
      * The preload function
-     * @return {void}
      */
-    preload () {
-        // console.log('preload');
+    preload() {
+        // Nothing here at the moment
     }
 
     /**
      * The create function
-     * @return {void}
      */
-    create () {
-        // console.log('create');
+    create() {
+        this._buildMenu();
     }
 
     /**
      * The update function
-     * @return {void}
      */
-    update () {
-        // console.log('update');
+    update() {
+        // Nothing here at the moment
+    }
+
+    /* **************************** */
+    /* ****** PRIVATE MEMBERS ***** */
+    /* **************************** */
+
+    /**
+     * Builds the main manu
+     * @private
+     */
+    _buildMenu() {
+
+        // The text menu config
+        let centerX     = this._game.config.width / 2,
+            textConfig  = {
+                fontSize   : '40px',
+                fill       : '#fff',
+                fontFamily : 'Arial'
+            },
+            titleConfig = {
+                fontSize   : '100px',
+                fill       : '#ff0000',
+                fontFamily : 'Arial'
+            };
+
+        let title = this.add.text(centerX, 50, "Feel", titleConfig);
+        title.setOrigin(0.5, 0);
+
+        let play = this.add.text(centerX, 250, "Play", textConfig);
+        play.setOrigin(0.5, 0)
+            .setInteractive()
+            .on('pointerdown', this._clickPlay);
+
+        let options = this.add.text(centerX, 325, "Options", textConfig);
+        options.setOrigin(0.5, 0)
+            .setInteractive()
+            .on('pointerdown', this._clickOptions);
+    }
+
+    /**
+     * Click on play in the menu
+     * @private
+     */
+    _clickPlay() {
+        /*
+         * `this` here refers to the 'Play' text object
+         */
+        console.log(this);
+        this.setFill('#00f');
+    }
+
+    /**
+     * Click on options in the menu
+     * @private
+     */
+    _clickOptions() {
+        console.log('TODO: click options');
     }
 }
