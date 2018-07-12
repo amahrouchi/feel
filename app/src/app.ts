e041f9055b39bca4027e5499f3a6872cd6864965
Angelo Mahrouchi <angelo.mahrouchi@omictools.com>
2018-07-12 14:11:57 +0200

    Example in TS (not working)

--- app/src/app.ts
+++ app/src/app.ts
@@ -4,18 +4,13 @@
 
 /// <reference path="types/phaser.d.ts"/>
 import 'phaser';
-import {A} from './tests/test';
-import {B} from './tests/test2';
-
-let a = new A();
-a.test();
-
-let b = new B();
-b.test();
+import {Test} from "./scenes/test";
 
 /* *********** */
 /* TEST PHASER */
 /* *********** */
+let example = new Test();
+
 let config = {
     type: Phaser.AUTO,
     width: 800,
@@ -26,40 +21,7 @@ let config = {
             gravity: { y: 200 }
         }
     },
-    scene: {
-        preload: preload,
-        create: create
-    }
+    scene: example
 };
 
 let game = new Phaser.Game(config);
-
-function preload ()
-{
-    this.load.setBaseURL('http://labs.phaser.io');
-
-    this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
-}
-
-function create ()
-{
-    this.add.image(400, 300, 'sky');
-
-    let particles = this.add.particles('red');
-
-    let emitter = particles.createEmitter({
-        speed: 100,
-        scale: { start: 1, end: 0 },
-        blendMode: 'ADD'
-    });
-
-    let logo = this.physics.add.image(400, 100, 'logo');
-
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
-
-    emitter.startFollow(logo);
-}
--- app/src/tests/test2.ts
+++
@@ -1,6 +0,0 @@
-export class B {
-    test () {
-        console.log('class B');
-        console.log('class B2');
-    }
-}
