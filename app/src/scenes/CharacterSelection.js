09675cfdf785c519f7aae28069246a696800c5b7
Angelo Mahrouchi <angelo.mahrouchi@omictools.com>
2018-07-13 13:32:06 +0200

    Prepare the character selection scene

---
+++ app/src/scenes/CharacterSelection.js
@@ -0,0 +1,35 @@
+import 'phaser';
+
+/**
+ * Character selection scene
+ */
+export default class CharacterSelection extends Phaser.Scene
+{
+    /**
+     * CharacterSelection scene constructor
+     */
+    constructor() {
+        super({'key' : 'CharacterSelection'});
+    }
+
+    /**
+     * The preload function
+     */
+    preload() {
+        // Nothing here ATM
+    }
+
+    /**
+     * The create function
+     */
+    create() {
+        // Nothing here ATM
+    }
+
+    /**
+     * The update function
+     */
+    update() {
+        // Nothing here ATM
+    }
+}
