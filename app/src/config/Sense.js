eeb8888fe81fc497c9dd99106ede31f9cdd8ca11
Angelo Mahrouchi <angelo.mahrouchi@gmail.com>
2018-07-15 13:11:56 +0200

    Define senses constants

---
+++ app/src/config/Sense.js
@@ -0,0 +1,46 @@
+/**
+ * Senses configuration
+ */
+export default class Sense {
+
+    /**
+     * Generic name for the sight
+     * @returns {string}
+     */
+    static get SIGHT() {
+        return 'sight';
+    }
+
+    /**
+     * Generic name for the smell
+     * @returns {string}
+     */
+    static get SMELL() {
+        return 'smell';
+    }
+
+    /**
+     * Generic name for the taste
+     * @returns {string}
+     */
+    static get TASTE() {
+        return 'taste';
+    }
+
+    /**
+     * Generic name for the sound
+     * @returns {string}
+     */
+    static get SOUND() {
+        return 'sound';
+    }
+
+    /**
+     * Generic name for the touch
+     * @returns {string}
+     */
+    static get TOUCH() {
+        return 'touch';
+    }
+
+}
