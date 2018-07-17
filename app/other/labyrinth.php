7950ea701767c31e6694b0af3e3f9b7314e3c275
Angelo Mahrouchi <angelo.mahrouchi@omictools.com>
2018-07-17 10:31:37 +0200

    Labyrinth generation and display algorithms into functions

--- app/other/labyrinth.php
+++ app/other/labyrinth.php
@@ -1,92 +1,107 @@
 <?php
-$width = 41;
-$center = 21;
-$line = array_fill(0,$width,0);
-$matrix = array_fill(0,$width, $line);
-$matrix[$center][$center] = 1;
-$matrix[$center][$center + 1] = 1;
-$matrix[$center][$center + 2] = 1;
-$matrix[$center][$center - 1] = 1;
-$matrix[$center][$center - 2] = 1;
-$matrix[$center + 1][$center] = 1;
-$matrix[$center + 2][$center] = 1;
-$matrix[$center - 1][$center] = 1;
-$matrix[$center - 2][$center] = 1;
 
-$nodes = [
-    [$center, $center + 2],
-    [$center, $center - 2],
-    [$center + 2, $center],
-    [$center - 2, $center],
-];
 
-$nodeKey = 0;
-$count = 0;
-while (isset($nodes[$nodeKey])) {
-    // foreach ($nodes as $nodeKey => $node) {
-    $node = $nodes[$nodeKey];
+function matrix($width, $center, $rngRatio)
+{
+    $line                         = array_fill(0, $width, 0);
+    $matrix                       = array_fill(0, $width, $line);
+    $matrix[$center][$center]     = 1;
+    $matrix[$center][$center + 1] = 1;
+    $matrix[$center][$center + 2] = 1;
+    $matrix[$center][$center - 1] = 1;
+    $matrix[$center][$center - 2] = 1;
+    $matrix[$center + 1][$center] = 1;
+    $matrix[$center + 2][$center] = 1;
+    $matrix[$center - 1][$center] = 1;
+    $matrix[$center - 2][$center] = 1;
 
-    // Current coordinates
-    $x = $node[0];
-    $y = $node[1];
-
-    // Coordinates for cell around the current one
-    $around = [
-        [$x, $y + 2], // ponderer en fonction de la direction (50% tout droit, 25% virage)
-        [$x, $y - 2],
-        [$x + 2, $y],
-        [$x - 2, $y],
+    $nodes = [
+        [$center, $center + 2],
+        [$center, $center - 2],
+        [$center + 2, $center],
+        [$center - 2, $center],
     ];
 
-    // Iterate on cells around
-    foreach ($around as $nextCell) {
-        // Cell coords
-        $nextCellX = $nextCell[0];
-        $nextCellY = $nextCell[1];
+    $nodeKey = 0;
+    $count   = 0;
+    while (isset($nodes[$nodeKey])) {
+        // foreach ($nodes as $nodeKey => $node) {
+        $node = $nodes[$nodeKey];
 
-        // Unavailable cell
-        if (
-            !isset($matrix[$nextCellX][$nextCellY])
-            || $matrix[$nextCellX][$nextCellY] === 1
-        ) {
-            continue;
-        }
+        // Current coordinates
+        $x = $node[0];
+        $y = $node[1];
 
-        // Define if the path continues
-        if ($matrix[$nextCellX][$nextCellY] !== 1) {
-            $rng = rand(0, 99);
-            if ($rng < 50) {
-                $matrix[$nextCellX][$nextCellY] = 1;
+        // Coordinates for cell around the current one
+        $around = [
+            [$x, $y + 2], // ponderer en fonction de la direction (50% tout droit, 25% virage)
+            [$x, $y - 2],
+            [$x + 2, $y],
+            [$x - 2, $y],
+        ];
 
-                if ($nextCellX !== $x) {
-                    $a = ($nextCellX + $x) / 2;
-                    $matrix[$a][$nextCellY] = 1;
+        // Iterate on cells around
+        foreach ($around as $nextCell) {
+            // Cell coords
+            $nextCellX = $nextCell[0];
+            $nextCellY = $nextCell[1];
+
+            // Unavailable cell
+            if (
+                !isset($matrix[$nextCellX][$nextCellY])
+                || $matrix[$nextCellX][$nextCellY] === 1
+            ) {
+                continue;
+            }
+
+            // Define if the path continues
+            if ($matrix[$nextCellX][$nextCellY] !== 1) {
+                $rng = rand(0, 99);
+                if ($rng < $rngRatio) {
+                    $matrix[$nextCellX][$nextCellY] = 1;
+
+                    if ($nextCellX !== $x) {
+                        $a                      = ($nextCellX + $x) / 2;
+                        $matrix[$a][$nextCellY] = 1;
+                    }
+
+                    if ($nextCellY !== $y) {
+                        $a                      = ($nextCellY + $y) / 2;
+                        $matrix[$nextCellX][$a] = 1;
+                    }
+
+                    $nodes[] = [$nextCellX, $nextCellY];
+
+                    $count++;
                 }
-
-                if ($nextCellY !== $y) {
-                    $a = ($nextCellY + $y) / 2;
-                    $matrix[$nextCellX][$a] = 1;
-                }
-
-                $nodes[] = [$nextCellX, $nextCellY];
-
-                $count++;
             }
         }
+
+        // All cells are unavailable => remove the node
+        unset($nodes[$nodeKey]);
+        $nodeKey++;
     }
 
-    // $stop var is useless ATM
+    echo $count . PHP_EOL;
 
-    // All cells are unavailable => remove the node
-    unset($nodes[$nodeKey]);
-    $nodeKey++;
+    return ['count' => $count, 'matrix' => $matrix];
 }
 
-foreach($matrix as $line) {
-    foreach($line as $cell) {
-        echo $cell === 1 ? "\e[47m \e[49m" : "\e[44m \e[49m";
+function displayMatrix($matrix)
+{
+    foreach ($matrix as $line) {
+        foreach ($line as $cell) {
+            echo $cell === 1 ? "\e[47m \e[49m" : "\e[44m \e[49m";
+        }
+        echo PHP_EOL;
     }
-    echo PHP_EOL;
 }
 
-echo $count . PHP_EOL;
+$count  = 0;
+$matrix = [];
+while ($count <= 250) {
+    $data   = matrix(41, 21, 45);
+    $count  = $data['count'];
+    $matrix = $data['matrix'];
+}
+displayMatrix($matrix);
