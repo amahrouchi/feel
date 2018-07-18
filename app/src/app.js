9700a9374593f498bb2b907c785f842c88f9f037
Angelo Mahrouchi <angelo.mahrouchi@omictools.com>
2018-07-18 14:35:39 +0200

    Test the labyrinth generation and display it in the console

--- app/src/app.js
+++ app/src/app.js
@@ -4,6 +4,7 @@ import MainMenu from './scenes/MainMenu';
 import CharacterSelection from "./scenes/CharacterSelection";
 import Game from "./scenes/Game";
 import CanvasResize from "./misc/CanvasResize";
+import Labyrinth from "./maps/Labyrinth";
 
 // Prepare the default scene
 config.scene = [
@@ -21,3 +22,18 @@ for (let sceneItem of config.scene) {
 // Init the canvas resizing
 let resizer = new CanvasResize(game);
 resizer.init();
+
+// ------------------------------------------
+// TEST: display the labyrinth in the console
+// ------------------------------------------
+let lab = new Labyrinth();
+let matrix = lab.generate(41, 21, 45, 10, 290);
+for (let line of matrix) {
+    let line2 = [];
+    for (let val of line) {
+        line2.push(
+            val === 1 ? val : ' '
+        );
+    }
+    console.log(...line2);
+}
