2b113bfa9e50c23ecc418f08aa5bc03b77aa36d0
Angelo Mahrouchi <angelo.mahrouchi@omictools.com>
2018-07-18 14:29:45 +0200

    Port the PHP algorithm to JS

---
+++ app/src/maps/Labyrinth.js
@@ -0,0 +1,171 @@
+export default class Labyrinth {
+
+    /**
+     * Generates a complex labyrinth
+     * @param {int} width     Labyrinth width
+     * @param {int} center    Labyrinth center coordinate
+     * @param {int} pathRatio The ratio used to create a new adjacent cell
+     * @param {int} loopRatio The ratio used to create a loop in the labyrinth
+     * @returns {Array}
+     */
+    generate(width, center, pathRatio, loopRatio) {
+        let count  = 0;
+        let matrix = [];
+        while (count <= 290) { // 290 represents the number of cells created in the labyrinth (its complexity)
+            let data = this._generate(width, center, pathRatio, loopRatio);
+            count    = data.count;
+            matrix   = data.matrix;
+        }
+
+        return matrix;
+    }
+
+
+    /**
+     * Generates a labyrinth
+     * @param {int} width     Labyrinth width
+     * @param {int} center    Labyrinth center coordinate
+     * @param {int} pathRatio The ratio used to create a new adjacent cell
+     * @param {int} loopRatio The ratio used to create a loop in the labyrinth
+     * @returns {{count: number, matrix: []}}
+     * @private
+     */
+    _generate(width, center, pathRatio, loopRatio) {
+
+        // Init the labyrinth
+        let matrix = this._initMatrix(width, center);
+
+        // Init the nodes
+        let nodes = this._initNodes(center);
+
+        let nodeKey = 0;
+        let count   = 0;
+        while (typeof(nodes[nodeKey]) !== 'undefined') {
+            // Init current node data
+            let node = nodes[nodeKey];
+            let x    = node[0];
+            let y    = node[1];
+
+            // Coordinates for cell around the current one
+            let around = [
+                [x, y + 2],
+                [x, y - 2],
+                [x + 2, y],
+                [x - 2, y],
+            ];
+
+            // Iterate on cells around
+            for (let nextCell of around) {
+
+                // Cell coords
+                let nextCellX = nextCell[0];
+                let nextCellY = nextCell[1];
+
+                // Unavailable cell
+                if (typeof(matrix[nextCellX]) === 'undefined' || typeof(matrix[nextCellX][nextCellY]) === 'undefined') {
+                    continue;
+                }
+
+                // Loop creation
+                if (matrix[nextCellX][nextCellY] === 1) {
+                    let rng = Math.random() * 100;
+                    if (rng >= loopRatio) {
+                        continue;
+                    }
+                }
+
+                // Define if the path continues
+                let rng = Math.random() * 100;
+                if (rng < pathRatio) {
+                    let newNode = false;
+                    if (matrix[nextCellX][nextCellY] === 0) {
+                        matrix[nextCellX][nextCellY] = 1;
+                        newNode                      = true;
+                    }
+
+                    // Cell between the node and the new created cell
+                    // TODO: factorize
+                    if (nextCellX !== x) {
+                        let xBetween                = (nextCellX + x) / 2;
+                        matrix[xBetween][nextCellY] = 1;
+                    }
+                    if (nextCellY !== y) {
+                        let yBetween                = (nextCellY + y) / 2;
+                        matrix[nextCellX][yBetween] = 1;
+                    }
+
+                    // Add a new node at the end of the nodes array
+                    if (newNode) {
+                        nodes.push([nextCellX, nextCellY]);
+                    }
+
+                    count++;
+                }
+            }
+
+            // Remove the current node
+            delete nodes[nodeKey];
+
+            nodeKey++;
+        }
+
+        return {
+            'count'  : count,
+            'matrix' : matrix
+        };
+    }
+
+    /**
+     * Initializes the matrix
+     * @param {int} width
+     * @param {int} center
+     * @returns {[]}
+     * @private
+     */
+    _initMatrix(width, center) {
+        let matrix                 = this._emptyMatrix(width);
+        matrix[center][center]     = 1;
+        matrix[center][center + 1] = 1;
+        matrix[center][center + 2] = 1;
+        matrix[center][center - 1] = 1;
+        matrix[center][center - 2] = 1;
+        matrix[center + 1][center] = 1;
+        matrix[center + 2][center] = 1;
+        matrix[center - 1][center] = 1;
+        matrix[center - 2][center] = 1;
+
+        return matrix;
+    }
+
+    /**
+     * Generates an empty matrix
+     * @param width
+     * @returns {Array}
+     * @private
+     */
+    _emptyMatrix(width) {
+        let matrix = [];
+        for (let i = 0; i < width; i++) {
+            let line = Array(width).fill(0, 0);
+            matrix.push(line);
+        }
+
+        return matrix;
+    }
+
+    /**
+     * Initializes the nodes
+     * @param {int} center
+     * @returns {Array}
+     * @private
+     */
+    _initNodes(center) {
+        return [
+            [center, center + 2],
+            [center, center - 2],
+            [center + 2, center],
+            [center - 2, center],
+        ];
+    }
+
+}
