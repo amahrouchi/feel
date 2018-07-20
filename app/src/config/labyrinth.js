8b61c605d88c739be98c0136eb04163c463a8b96
Angelo Mahrouchi <angelo.mahrouchi@omictools.com>
2018-07-20 10:40:53 +0200

    Labyrinth config file

---
+++ app/src/config/labyrinth.js
@@ -0,0 +1,31 @@
+export default {
+    /**
+     * The labyrinth width and height
+     * @var {int}
+     */
+    WIDTH : 41,
+
+    /**
+     * The labyrinth center position
+     * @var {int}
+     */
+    CENTER : 21,
+
+    /**
+     * The ratio used to generate the labyrinth path
+     * @var {int}
+     */
+    PATH_RATIO : 50,
+
+    /**
+     * The ratio used to generate the labyrinth loops
+     * @var {int}
+     */
+    LOOP_RATIO : 10,
+
+    /**
+     * The complexity of the labyrinth
+     * @var {int}
+     */
+    COMPLEXITY : 290
+}
