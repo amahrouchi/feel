317cbd65c29cdf081f0847f7af08704d83d5b2c1
Angelo Mahrouchi <angelo.mahrouchi@gmail.com>
2018-08-25 06:49:03 +0200

    Refactor the player's creation

--- app/src/scenes/Game.js
+++ app/src/scenes/Game.js
@@ -73,13 +73,7 @@ export default class Game extends AbstractScene
         this._cursors = this.input.keyboard.createCursorKeys();
 
         // Player
-        let positions = this._playerPosition();
-        this._player  = this.physics.add.sprite(positions.x, positions.y, 'walkFront0');
-        this._player.setScale(SenseConfig.PLAYER_SPRITE_RATIO);
-        this._player.body.setSize(SenseConfig.PLAYER_HITBOX_WIDTH, SenseConfig.PLAYER_HITBOX_HEIGHT);
-        this._player.body.setOffset(SenseConfig.PLAYER_HITBOX_OFFSET_X, SenseConfig.PLAYER_HITBOX_OFFSET_Y);
-        this._player.setCollideWorldBounds(true);
-        this.physics.add.collider(this._layers.wallsLayer, this._player);
+        this._player  = this._sense.create();
 
         // Camera configuration
         this.cameras.main.startFollow(this._player);
@@ -144,50 +138,17 @@ export default class Game extends AbstractScene
     _createSense(sense) {
         switch (sense) {
             case SenseConfig.SIGHT:
-                return new Sight();
+                return new Sight(this);
             case SenseConfig.SOUND:
-                return new Sound();
+                return new Sound(this);
             case SenseConfig.SMELL:
-                return new Smell();
+                return new Smell(this);
             case SenseConfig.TOUCH:
-                return new Touch();
+                return new Touch(this);
             case SenseConfig.TASTE:
-                return new Taste();
+                return new Taste(this);
             default:
                 throw 'Unknown sense selected.'
         }
-    }
-
-    /**
-     * Determines the player position at the beginning of the game
-     * @returns {{x: number, y: number}}
-     * @private
-     */
-    _playerPosition() {
-
-        let x = 0,
-            y = 0;
-
-        // First cell of the matrix
-        break_block: {
-            y = 0;
-            for (let line of this._labyrinth.matrix) {
-                x = 0;
-                for (let cell of line) {
-                    if (cell === 1) {
-                        break break_block;
-                    }
-                    x++;
-                }
-                y++;
-            }
-        }
-
-        let cellSizeInPixels = LabyrinthConfig.TILE_SIZE * LabyrinthConfig.MAP_SIZE_RATIO;
-
-        return {
-            x : x * cellSizeInPixels + cellSizeInPixels / 2,
-            y : y * cellSizeInPixels + cellSizeInPixels / 2,
-        };
     }
 }
--- app/src/senses/AbstractSense.js
+++ app/src/senses/AbstractSense.js
@@ -1,3 +1,63 @@
+import SenseConfig from "../config/sense";
+import LabyrinthConfig from "../config/labyrinth";
+
 export default class AbstractSense {
 
+    /**
+     * Initializes the players
+     * @param {AbstractScene} scene
+     */
+    constructor(scene) {
+        this._scene = scene;
+        this._sprite = null;
+    }
+
+    /**
+     * Creates the player in the scene
+     * @return {Phaser.Physics.Arcade.Sprite}
+     */
+    create() {
+        let positions = this._playerPosition();
+        this._sprite  = this._scene.physics.add.sprite(positions.x, positions.y, 'walkFront0');
+        this._sprite.setScale(SenseConfig.PLAYER_SPRITE_RATIO);
+        this._sprite.body.setSize(SenseConfig.PLAYER_HITBOX_WIDTH, SenseConfig.PLAYER_HITBOX_HEIGHT);
+        this._sprite.body.setOffset(SenseConfig.PLAYER_HITBOX_OFFSET_X, SenseConfig.PLAYER_HITBOX_OFFSET_Y);
+        this._sprite.setCollideWorldBounds(true);
+        this._scene.physics.add.collider(this._scene._layers.wallsLayer, this._sprite);
+
+        return this._sprite;
+    }
+
+    /**
+     * Determines the player position at the beginning of the game
+     * @returns {{x: number, y: number}}
+     * @private
+     */
+    _playerPosition() {
+
+        let x = 0,
+            y = 0;
+
+        // First cell of the matrix
+        break_block: {
+            y = 0;
+            for (let line of this._scene._labyrinth.matrix) {
+                x = 0;
+                for (let cell of line) {
+                    if (cell === 1) {
+                        break break_block;
+                    }
+                    x++;
+                }
+                y++;
+            }
+        }
+
+        let cellSizeInPixels = LabyrinthConfig.TILE_SIZE * LabyrinthConfig.MAP_SIZE_RATIO;
+
+        return {
+            x : x * cellSizeInPixels + cellSizeInPixels / 2,
+            y : y * cellSizeInPixels + cellSizeInPixels / 2,
+        };
+    }
 }
